6362e809dd13986c630e5ada511233fbe87163cd
Kieran-Go <187461911+Kieran-Go@users.noreply.github.com>
2025-01-09 11:40:49 +1030

    Add modules to streamline DOM manipulation
    Added a domUtil.js file which contains functions that streamline the creation
    of certain DOM elements.
    Added a storageFunctions.js file which contains functions that streamline saving/loading
    to local storage.

---
+++ src/scripts/storageFunctions.js
@@ -0,0 +1,9 @@
+// Functions to manage local storage
+function saveToLocalStorage(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+  
+function loadFromLocalStorage(key) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+}
